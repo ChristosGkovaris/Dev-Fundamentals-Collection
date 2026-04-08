849fc79159d87daac28e59537aceed921fbb3e80
Christos Gkovaris <147056529+ChristosGkovarisCSE52@users.noreply.github.com>
2026-04-08 12:09:06 +0300

    UPD: new version of the gif program

--- src/python/programs/gif.py
+++ src/python/programs/gif.py
@@ -1,11 +1,8 @@
-# pip3 install imageio
-
 import imageio.v3 as iio
+import glob
 
-filenames = ['team-pic1.png', 'team-pic2.png']
-images = [ ]
-
-for filename in filenames:
-  images.append(iio.imread(filename))
+# This grabs every .png file in the folder automatically
+filenames = glob.glob('team-pic*.png')
+images = [iio.imread(f) for f in filenames]
 
 iio.imwrite('team.gif', images, duration = 500, loop = 0)
